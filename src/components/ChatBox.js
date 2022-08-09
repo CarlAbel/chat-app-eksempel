91d1cd851449e57d4dcc128c401915f40580973d
CarlAbel <caweb.cph@gmail.com>
2022-08-09 15:27:05 +0200

    last save of the day

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,42 +1,50 @@
-
 import { useState, useContext, useEffect } from "react"
 import { MessageContext } from "../MessageContext"
+import { OnlineContext } from "../OnlineContext"
 
-export default function ChatBox({ isWriting, setIsWriting }) {
+export default function ChatBox({ user, isWriting, setIsWriting }) {
   const [text, setText] = useState("")
   const [selfTyping, setSelfTyping] = useState(false)
 
+  const [setIsOnline, isOnline] = useContext(OnlineContext)
   const { setMessages } = useContext(MessageContext)
 
   useEffect(
     function () {
-      setTimeout(
-        function () {
-          setIsWriting(false)
-          setSelfTyping(false)
-        },
-        [2000]
-      )
+      setTimeout(function () {
+        setIsWriting(false)
+        setSelfTyping(false)
+      }, 1000)
+      setTimeout(function () {
+        setIsOnline(
+          isOnline.map((item) =>
+            item.user === user ? { ...item, isOnline: false } : item
+          )
+        )
+      }, 1000)
     },
     [isWriting]
   )
 
   function sendMessage(event) {
-    //const tempArray = [...messages, { message: text }]
-    //setMessages(tempArray)
-    setMessages((previousState) => [...previousState, { message: text }])
+    setMessages((previousState) => [...previousState, { user, message: text }])
+    setText("")
   }
 
   return (
     <>
       {isWriting && !selfTyping ? <div>...</div> : null}
-
       <input
         type="text"
         value={text}
         onKeyUp={() => {
           setIsWriting(true)
           setSelfTyping(true)
+          setIsOnline(
+            isOnline.map((item) =>
+              item.user === user ? { ...item, isOnline: true } : item
+            )
+          )
         }}
         onChange={(e) => setText(e.target.value)}
       />
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,26 +1,36 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
+import { useContext } from "react"
+import { OnlineContext } from "../OnlineContext"
 
-const styles = {
-  container: css`
-    display: flex;
-    flex-direction: row-reverse;
-  `,
-  bubble: css`
-    border-radius: 50%;
-    height: 30px;
-    width: 30px;
-    background-color: goldenrod;
-  `,
-  text: css`
-    background-color: lightblue;
-    padding: 5em 1em;
-    border-radius: 7px;
-    border-bottom-left-radius: 0;
-  `,
-}
+export default function Message({ text, user, thisIs }) {
+  const [isOnline] = useContext(OnlineContext)
 
-export default function Message({ text }) {
+  const isMe = thisIs === user
+  var isOtherOnline = false
+
+  isOnline.forEach((item) =>
+    item.user !== user ? (isOtherOnline = item.isOnline) : null
+  )
+
+  const styles = {
+    container: css`
+      display: flex;
+      flex-direction: row ${isMe ? "-reverse" : null};
+    `,
+    bubble: css`
+      border-radius: 50%;
+      height: 30px;
+      width: 30px;
+      background-color: ${isOtherOnline ? "green" : "yellow"};
+    `,
+    text: css`
+      background-color: lightblue;
+      padding: 0.5em 1em;
+      border-radius: 7px;
+      border-bottom-left-radius: 0;
+    `,
+  }
   return (
     <div css={styles.container}>
       <div css={styles.bubble}></div>
